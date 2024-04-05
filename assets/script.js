c48aa28c0b02a6e3dda494d76662522a7e529d7e
mehrnazkiani24 <mehrnaz03@yahoo.com>
2024-04-05 11:00:24 -0700

    added 5day and current forecast

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ searchbutton.addEventListener("click",function(event){
     var city = cityname.value
     console.log(city)
     getforcast(city)
+    Get5dayforcast(city)
 })
 
 function getforcast(city){
@@ -15,6 +16,43 @@ function getforcast(city){
     .then(apiResponse => apiResponse.json())
     .then(result =>{
         console.log(result)
+        var html = `<div class="card" style="width: 18rem;">
+        <img src="https://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png" class="card-img-top" alt="icon">
+        <div class="card-body">
+          <h5 class="card-title">City ${result.name}</h5>
+          <p class="card-text">Description: ${result.weather[0].description}</p>
+          <p class="card-text">Temprature: ${result.main.temp}</p>
+          <p class="card-text">Humidity: ${result.main.humidity}</p>
+          <p class="card-text">Windspeed: ${result.wind.speed}</p>
+       
+        </div>
+      </div>`
+document.getElementById("currentforecast").innerHTML=html
     })
 }
 
+function Get5dayforcast(city){
+    //api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+    var url=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}&units=imperial`
+    console.log(url)
+    fetch(url)
+    .then(apiResponse => apiResponse.json())
+    .then(result =>{
+        console.log(result)
+        var html=""
+        for(let i=0;i<result.list.length;i++){
+            html+= `<div class="card" style="width: 18rem;">
+            <img src="https://openweathermap.org/img/wn/${result.list[i].weather[0].icon}@2x.png" class="card-img-top" alt="icon">
+            <div class="card-body">
+              <h5 class="card-title"> ${result.list[i].dt_txt}</h5>
+              <p class="card-text">Description: ${result.list[i].weather[0].description}</p>
+              <p class="card-text">Temprature: ${result.list[i].main.temp}</p>
+              <p class="card-text">Humidity: ${result.list[i].main.humidity}</p>
+              <p class="card-text">Windspeed: ${result.list[i].wind.speed}</p>
+           
+            </div>
+          </div>`
+        }
+        document.getElementById("5dayforecast").innerHTML=html
+    })
+}
